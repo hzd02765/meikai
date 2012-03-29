b5508c37f2ce48d4d96c8cec9198bc016d824db0
sakai tomoyuki from Ubuntu <hzd02765@nifty.com>
2012-03-29 16:11:25 +0900

    commit

---
+++ list_02/list_02_10.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int i, n;
+  int prime[500];
+  int ptr = 0;
+  unsigned long counter = 0;
+
+  prime[ptr++] = 2;
+  //prime[ptr++] = 3;
+
+  for(n = 2; n <= 1000; n++){
+    for(i = 1; i < ptr; i++){
+      counter++;
+      if(n % prime[i] == 0){
+	break;
+      }
+    }
+    if(ptr == i){
+      prime[ptr++] = n;
+    }
+  }
+  
+  for(i = 0; i < ptr; i++){
+    printf("%d\n", prime[i]);
+  }
+
+  printf("除算を行った回数：%lu\n", counter);
+
+  return (0);
+}
---
+++ list_02/list_02_13.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define VMAX 21
+
+typedef struct{
+  char name[20];
+  int height;
+  double vision;
+}PhysCheck;
+
+double ave_height(const PhysCheck dat[], int n)
+{
+
+}
+
+void dist_vision(const PhysCheck dat[], int n, int dist[])
+{
+
+}
+
+int main(void)
+{
+
+
+  return (0);
+}
