ada1b4415bdbee7867a0de452e797d87bed72239
sakai tomoyuki from Ubuntu <hzd02765@nifty.com>
2012-04-04 11:10:44 +0900

    commit

---
+++ list_03/list_03_06.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int sum(int x1, int x2)
+{
+  return (x1 + x2);
+}
+
+int mul(int x1, int x2)
+{
+  return (x1 * x2);
+}
+
+void kuku(int (*calc)(int, int))
+{
+  int i ,j;
+
+  for(i = 1; i <= 9; i++){
+    for(j = 1; j <= 9; j++){
+      printf("%3d", (*calc)(i, j));
+    }
+    putchar('\n');
+  }
+}
+
+int main(void)
+{
+  puts("九九の足し算表");
+  kuku(sum);
+
+  puts("\n九九の掛け算表");
+  kuku(mul);
+
+  return (0);
+}
---
+++ list_03/list_03_07.c
@@ -0,0 +1,10 @@
+int int_cmp(const void *a, const void *b)
+{
+  if(*(int *)a < *(int *)b){
+    return (-1);
+  }else if(*(int *)a > *(int *)b){
+    return (1);
+  }else{
+    return (0);
+  }
+}
