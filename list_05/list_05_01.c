cd6cbd7db6e2270b79d0b18bcc3ddb8a4de1cb42
tomoyuki@Ubuntu <hzd02765@nifty.com>
2012-04-20 17:18:34 +0900

    再帰について
    再帰関数呼び出し
    ユークリッドの互除法

---
+++ list_05/list_05_01.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+  if(n > 0){
+    return (n * factorial(n - 1));
+  } else{
+   return (1);
+ }
+
+}
+
+int main(void)
+{
+    int x;
+
+    printf("整数を入力せよ：");
+    scanf("%d", &x);
+
+    printf("%dの階乗は%dです。\n", x, factorial(x));
+
+    return (0);
+ }
---
+++ list_05/list_05_02.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int gcd(int x, int y)
+{
+  if(y == 0){
+    return (x);
+  }else{
+    return (gcd(y, x % y));
+  }
+}
+
+int main(void)
+{
+  int x, y;
+
+  printf("整数を入力せよ：");
+  scanf("%d", &x);
+
+  printf("整数を入力せよ：");
+  scanf("%d", &y);
+
+  printf("最大公約数は%dです。\n", gcd(x, y));
+
+  return (0);
+}
